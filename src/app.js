34c9c62aa54c96ebec06dd8e5f25d3e96b3c0c9c
Anish-Rawat <anishrawat53@gmail.com>
2025-04-08 10:53:07 +0530

    added user and video model

--- src/app.js
+++ src/app.js
@@ -1,5 +1,17 @@
 import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
 
 const app = express()
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials:true,
+}))
+
+app.use(express.json({limit:"16kb"}))
+app.use(express.urlencoded({extended:true,limit:"16kb"})) // it not required dont use extended
+app.use(express.static("public"))   // here sttic is folder that is public outisde src folder
+app.use(cookieParser())
 
 export default app
---
+++ src/models/video.model.js
@@ -0,0 +1,45 @@
+import mongoose, {Schema} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new Schema(
+    {
+        videoFile:{
+            type: String , // Cloudinary Url
+            required:true,
+        },
+        thumbnail: {
+            type: String , // Cloudinary Url
+            required:true,
+        },
+        title: {
+            type: String ,
+            required:true,
+        },
+        description: {
+            type: String ,
+            required:true,
+        },
+        duration: {
+            type: Number , 
+            required:true,
+        },
+        views: {
+            type: Number , // Cloudinary Url
+            default:0,
+        },
+        ispublished:{
+            type:Boolean,
+            default:true,
+        },
+        owner: {
+            type: Schema.Types.ObjectId , // Cloudinary Url
+            ref:"User",
+        }
+    },
+    {
+        timestamps:true,
+    }
+)
+
+videoSchema.plugin(mongooseAggregatePaginate)
+export const Video = mongoose.model("Video",videoSchema)
