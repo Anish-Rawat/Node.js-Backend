823dd8d9c5f15a3bd438aefee423d6566ea8215a
Anish-Rawat <anishrawat53@gmail.com>
2025-04-10 11:43:45 +0530

    Register user in mongodb

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -54,7 +54,7 @@ const userSchema = new Schema(
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
 
-  this.password = bcrypt.hash(this.password, 10);
+  this.password = await bcrypt.hash(this.password, 10);
   next();
 }); // in pre dont use arrow fucniton ad call back their will be problem on htis , arrow funciton dont hav ethis reference context and here context is required
 
